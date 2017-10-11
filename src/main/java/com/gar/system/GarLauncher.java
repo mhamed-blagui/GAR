f0742f4e5f03573a88d1b5992f5f5016bb9cda34
mhamed-blagui <mhamed.blagui88@gmail.com>
2017-10-11 19:53:02 +0100

    add conf

--- src/main/java/com/gar/system/GarLauncher.java
+++ src/main/java/com/gar/system/GarLauncher.java
@@ -1,8 +1,27 @@
 package com.gar.system;
 
+import java.util.Arrays;
+
+import org.apache.log4j.Logger;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootConfiguration
+@Configuration
+@ComponentScan
 public class GarLauncher {
 
+	private static Logger log = Logger.getLogger(GarLauncher.class);
+
+	public static void main(String[] args) {
+		final ApplicationContext ctx = SpringApplication.run(GarLauncher.class, args);
+		final String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (final String beanName : beanNames) {
+			log.info(beanName);
+		}
+	}
 }
